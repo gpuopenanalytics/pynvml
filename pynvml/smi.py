f7e5c157efbc4e3b84419cefea6d05c2da79df9d
rjzamora <rzamora217@gmail.com>
2024-07-11 07:40:51 -0700

    remove comma

--- pynvml/smi.py
+++ pynvml/smi.py
@@ -4,7 +4,7 @@ from nvml_utils.smi import *
 # Warn the user that this is the wrong module path
 warnings.warn(
     "The pynvml.smi module is deprecated and will be removed "
-    "in the next release of pynvml. Please use nvml_utils:",
+    "in the next release of pynvml. Please use nvml_utils:"
     "\n(e.g. `from nvml_utils import nvidia_smi`)",
     FutureWarning,
 )
